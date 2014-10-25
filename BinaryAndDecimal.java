96e95249de4423df861342a7020b958fe1710ebb
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-10-25 13:22:57 +0100

    added description

--- BinaryAndDecimal.java
+++ BinaryAndDecimal.java
@@ -1,3 +1,21 @@
+/*
+ Create a program in which you define the following methods:
+binary2decimal(String): takes from the user a binary number (with digits 0 and 1) and returns the corresponding
+number in decimal (base-10, with digits between 0 and 9). Hint: in the same way that you know that
+35 = 3 · 101 + 5 · 100
+, you can find that 100011 = 1 · 2
+5 + 1 · 2
+1 + 1 · 2
+0
+.
+decimal2binary(int): the opposite of the previous one: takes a decimal number and returns the corresponding
+binary number. Hint: instead of multiplying by 2, you will need to divide by two this time.
+The program must offer a menu to the user with two options. The first one takes a binary number from the
+user and returns the corresponding decimal number. The second one does the opposite: takes a decimal number
+and returns a binary number. The program should use the methods defined.
+  
+ */
+
 package day4;
 
 import java.util.Scanner;
