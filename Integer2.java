a970d384bca3be50eb5935b699a48f2e1c38c0c0
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-10-25 13:06:51 +0100

    initial

---
+++ Integer2.java
@@ -0,0 +1,32 @@
+/*
+ Create your own version of boxed int! Create a class Integer2 with only one field (int value) and the following
+methods:
+getValue(): returns the value of this number as an int, a getter.
+setValue(int): a setter.
+isEven(): returns true if the number is even, false otherwise.
+isOdd(): the opposite.
+prettyPrint(): prints the value of the integer on the screen.
+toString(): returns a String equivalent to the number 
+ 
+ */
+
+package day4;
+
+public class Integer2 {
+
+	int value;
+	
+	public Integer2 (int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		
+		int value = this.value;
+		
+		return value;
+	}
+	
+	
+	
+}
